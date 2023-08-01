2377b8e1d739d1bcb07ba0164f3ca5d170d3d861
Anna Galkevich <emikot321@gmail.com>
2023-08-01 22:28:40 +0300

    Added tests, Zustand

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,21 +1,80 @@
-import { useStore } from "../store";
-import "./Column.css";
-import Task from "./Task";
+import { useStore } from '../store';
+import Task from './Task';
+import './Column.css';
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
+import { shallow } from 'zustand/shallow';
+import plus from '../assets/plus.svg';
 
 export default function Column({ state }) {
-  const tasks = useStore((store) =>
-    store.tasks.filter((task) => task.state === state)
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+  const [drop, setDrop] = useState(false);
+
+  const tasks = useStore(
+    (store) => store.tasks.filter((task) => task.state === state),
+    shallow
   );
+  const addTask = useStore((store) => store.addTask);
+  const setDraggedTask = useStore((store) => store.setDraggedTask);
+  const draggedTask = useStore((store) => store.draggedTask);
+  const moveTask = useStore((store) => store.moveTask);
 
   return (
-    <div className="column">
+    <div
+      className={classNames('column', { drop: drop })}
+      onDragOver={(e) => {
+        setDrop(true);
+        e.preventDefault();
+      }}
+      onDragLeave={(e) => {
+        setDrop(false);
+        e.preventDefault();
+      }}
+      onDrop={(e) => {
+        setDrop(false);
+        moveTask(draggedTask, state);
+        setDraggedTask(null);
+      }}
+    >
       <div className="titleWrapper">
         <p>{state}</p>
-        <button onClick={() => {}}>Add</button>
+        <img src={plus} onClick={() => setOpen(true)} />
       </div>
-      {tasks.map((task)=> (
-      <Task title={task.title} key={task.title} />
+      {tasks.map((task) => (
+        <Task title={task.title} key={task.title} />
       ))}
+      {open && (
+        <div className="Modal">
+          <div className="modalContent">
+            <input onChange={(e) => setText(e.target.value)} value={text} />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText('');
+                setOpen(false);
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
+}
+
+function RefTest() {
+  const ref = useRef();
+
+  useEffect(() => {
+    useStore.subscribe(
+      (store) => store.tasks,
+      (tasks) => {
+        ref.current = tasks;
+      }
+    );
+  }, []);
+
+  return ref.current;
 }
